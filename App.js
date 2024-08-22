40585956d607666475f84eb86f3bbbf861f27b7c
shokkun0407 <shoki1310@gmail.com>
2024-08-23 02:55:24 +0900

    addnewfiles

--- App.js
+++ App.js
@@ -1,15 +1,84 @@
-import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
+  FlatList,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
+import { Icon } from "react-native-elements";
 
 export default function App() {
+  const [taskText, setTaskText] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [isEditing, setIsEditing] = useState(null);
+
+  const handleSaveTask = () => {
+    if (!taskText.trim()) return;
+    if(isEditing) {
+      setTasks(
+        tasks.map((task) =>
+          task.id === isEditing ? {...task, text: taskText } : task
+        )
+      );
+      setIsEditing(null);
+    } else {
+      const newTask = { id: Date.now().toString(), text: taskText };
+      setTasks([...tasks, newTask]);  
+    }
+    setTaskText("");
+  };
+  
+  const handleEdit = (item) => {
+    setTaskText(item.text);
+    setIsEditing(item.id);
+  };
+  
+  const handleDelete = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const renderTask = ({ item }) => {
+    return (
+      <View style={styles.task}>
+        <Text style={styles.taskText}>{item.text}</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.editButton}
+            onPress={() => handleEdit(item)}
+          >
+            <Icon name="edit" color="#4caf50">
+              編集
+            </Icon>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.deleteButton}
+            onPress={() => handleDelete(item.id)}
+          >
+            <Icon name="delete" color="#f44336">
+              削除
+            </Icon>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Todoアプリ</Text>
+      <TextInput
+        placeholder="タスクを入力"
+        style={styles.input}
+        onChangeText={setTaskText}
+        value={taskText}
+      />
+      <TouchableOpacity style={styles.saveButton} onPress={handleSaveTask}>
+        <Text style={styles.saveButtonText}>{isEditing ? "編集" : "追加"}</Text>
+      </TouchableOpacity>
+
+      <FlatList data={tasks} renderItem={renderTask} />
     </View>
   );
 }
@@ -17,8 +86,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    padding: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "#ccceee",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 6,
+  },
+  saveButton: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadious: 5,
+    marginBottom: 20,
+  },
+  saveButtonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  task: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 20,
+    padding: 10,
+    backgroundColor: "#eeeeee",
+    borderRadius: 5,
+  },
+  taskText: {
+    maxWidth: "80%",
+  },
+  buttonContainer: {
+    flexDirection: "row",
   },
 });
